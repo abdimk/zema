292c8fed54bc37b2446ac4c720c60ff47bd87214
Abdisa merga <63399762+abdimk@users.noreply.github.com>
2021-05-23 05:00:57 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = "1762120957:AAFemumOmw0Qv_RljC6QfdYxp7lqtQkqyi4"
+APP_ID = 4711610
+API_HASH = "a5cf28cb0dd1ef2c3fac77a296302258"
 
 youtube_next_fetch = 0  # time in minute
 
 
 EDIT_TIME = 5
+
+'''
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+APP_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+'''
